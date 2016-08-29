4bcbe78a34bb86ffa89ab8448abcdcba8aafb267
ppflrs <jfloresu@gmail.com>
2016-08-29 12:24:54 +0300

    Add FASTA file output

--- count_reads.py
+++ count_reads.py
@@ -7,7 +7,7 @@ requiers SAM file to have and MD string (either inserted by aligner or added usi
 and (http://code.google.com/p/pysam/).
 Input is a bam/sam file sorted or a stream.
 gff file is not required.
-currently: count +1 if EITHER of the PE reads aligned. DOES NOT count BOTH. 
+currently: count +1 if EITHER of the PE reads aligned. DOES NOT count BOTH.
 add different options for counting? add the other arg parse options
 """
 
@@ -33,20 +33,20 @@ def main():
 	exe_parser.add_argument("--order", help="order of bam/sam: (n)ame or p(osition). default=n", type=str)
 	exe_parser.add_argument("--gff", help="GFF file", type=str)
 	exe_parser.add_argument("-o", "--out", help="name of counts output file.", type=str)
-	
+
 	args = exe_parser.parse_args()
 
 	paired_end = True
 	pe_order = 'n'
-	#min_read_len = 90.0  
-	
+	#min_read_len = 90.0
+
 	if args.verbose:
 		verbose = True
 	else:
 		verbose = False
-	
 
-	#max_clip_ = float(0.1) 
+
+	#max_clip_ = float(0.1)
 	#min_id = float(98.0)
 	min_score = 0
 
@@ -58,10 +58,11 @@ def main():
 		samoutfile = None
 	if args.out:
 		outfile = open(args.out, "w")
-		outfile_genes = args.out.replace('.tsv','.cds.tsv')
+		outfile_fasta_filename = args.out.replace('.tsv','.fasta')
+		outfile_fasta = open(outfile_fasta_filename, "w")
 	else:
 		outfile = None
-	
+
 	if args.min_len:
 		min_read_len  = args.min_len
 	else:
@@ -80,12 +81,12 @@ def main():
 		gff_file = args.gff
 	else:
 		sys.exit('Exiting. No GFF file specified.')
-	
+
 	features, counts = gff_reader(gff_file, 'CDS', 'product')
 
 	hit_dict = dict()
 	notaligned = 0
-	
+
 	if args.infile:
 		try: #try opening the file
 			if args.infile == '-': #get sam on a stream
@@ -124,10 +125,10 @@ def main():
 		print "no input file given. exiting..."
 		sys.exit(1)
 	read_counter = 0
-	for alignment in reader: 
+	for alignment in reader:
 		read_counter += 1
 		read1_name = ''
-		read_2_name = '' 
+		read_2_name = ''
 		if read_counter % 1000000 == 0 and verbose:
 			if verbose:
 				print read_counter, 'alignment pairs processed'
@@ -138,7 +139,10 @@ def main():
 			iv_seq = tuple()
 			if (alignment[0] is not None) and (alignment[0].aligned):
 				read_1_name = alignment[0].read.name
-				read_1_length = len(alignment[0].read.seq)
+				read_1_seq = alignment[0].read.seq
+				read_1_length = len(read_1_seq)
+				read_1_start = alignment[0].iv.start
+				read_1_end = alignment[0].iv.end
 				#print 'read_1_length', read_1_length, alignment[0].read.seq, alignment[0].original_sam_line
 				opt_1_fields = alignment[0].optional_fields
 				flag_1 = alignment[0].flag
@@ -165,7 +169,7 @@ def main():
 				else:
 					id_check_1 = True
 					iv_seq_good_1 = True
-				
+
 				if (int(read_1_length) >= int(min_read_len)) or (read_1_length == 1): #LOOK INTO THIS CONDITION? HOW TO GET LEN OF * IN SAM
 					#if float(percent_1_id) >= min_id:#float(args.min_id):
 					if id_check_1 == True:
@@ -182,8 +186,10 @@ def main():
 			#now the second read
 			if (alignment[1] is not None) and (alignment[1].aligned):
 				read_2_name = alignment[1].read.name
-				#print read_2_name, read_1_name
-				read_2_length = len(alignment[1].read.seq)
+				read_2_seq = alignment[1].read.seq
+				read_2_length = len(read_2_seq)
+				read_2_start = alignment[1].iv.start
+				read_2_end = alignment[1].iv.end
 				#print 'read_2_length', read_2_length, alignment[1].read.seq
 				opt_2_fields = alignment[1].optional_fields
 				flag_2 = alignment[1].flag
@@ -210,7 +216,7 @@ def main():
 				else:
 					id_check_2 = True
 					iv_seq_good_2 = True
-				
+
 				if (int(read_2_length) >= int(min_read_len)) or (read_2_length == 1):
 					#if float(percent_1_id) >= min_id:#float(args.min_id):
 					if id_check_2 == True:
@@ -233,10 +239,10 @@ def main():
 					if iv.chrom not in features.chrom_vectors:
 					# check if alignment feaure name in features from GFF file
 					# The name of a sequence (i.e., chromosome, contig, or the like).
-					# check the gff features dictionary	
+					# check the gff features dictionary
 						#print '!!!'
 						raise UnknownChrom
-					
+
 					for iv2,fs2 in features[iv].steps():
 						#print '==='
 						feature_set = feature_set.union(fs2)
@@ -244,9 +250,9 @@ def main():
 					temp_iv.append(iv.chrom) #get name of chromosom/contig both PE reads align to, append to list.
 				if len(feature_set) == 1:
 					count_read_gff = False
-					
+
 					feature_name = list(feature_set)[0]
-					
+
 					if iv_seq_good_1:
 						count_read_gff = True
 					elif iv_seq_good_2:
@@ -255,7 +261,7 @@ def main():
 					if count_read_gff == True:
 						counts[feature_name] += 1
 						#print list(feature_set)[0], '\t',counts[list(feature_set)[0]]
-						
+
 				#print temp_iv, 'TRY', iv_seq_good_1, iv_seq_good_2
 				temp_iv = set(temp_iv)
 				#if temp_iv[0]  ==  temp_iv[1]: #if the read maps to the same contig/chromosom then +1 only once
@@ -271,15 +277,29 @@ def main():
 										if iv_seq_good_2:
 												#hit_dict['']
 												if percent_1_id >= percent_2_id:
+														#print 'scenario 1'
 														outfile.write('\t'.join(map(str,[read_1_name + '.1', genome, percent_1_id, feature]))+'\n')
-														
+														fasta_entry = fasta_formatter(alignment[0], 1, feature, percent_2_id)
+														outfile_fasta.write(fasta_entry)
+
 												else:
+														#print 'scenario 2'
+														#print alignment[1].read.name, alignment[1].iv.chrom, alignment[1].read.seq, feature, alignment[1].iv.start, alignment[1].iv.end
 														outfile.write('\t'.join(map(str,[read_2_name + '.2', genome, percent_2_id, feature]))+'\n')
+														fasta_entry = fasta_formatter(alignment[1], 2, feature, percent_2_id)
+														#print fasta_entry
+														outfile_fasta.write(fasta_entry)
+
 										elif alignment[1] is None:
+												#print 'scenario 3'
 												outfile.write('\t'.join(map(str,[read_1_name + '.1', genome, percent_1_id, feature]))+'\n')
+												fasta_entry = fasta_formatter(alignment[0], 1, feature, percent_2_id)
+												outfile_fasta.write(fasta_entry)
 								elif iv_seq_good_2:
-									#outfile.write('\t'.join(map(str,[read_1_name, genome, percent_1_id]))+'\n')
+									#print 'scenario 4'
 									outfile.write('\t'.join(map(str,[read_2_name + '.2', genome, percent_2_id, feature]))+'\n')
+									fasta_entry = fasta_formatter(alignment[1], 2, feature, percent_2_id)
+									outfile_fasta.write(fasta_entry)
 			except Exception, e:
 				pass
 			"""
@@ -289,6 +309,20 @@ def main():
 						if counts[gene] > 0:
 							outfile_genes_dict.write('\t'.join(map(str,[gene,counts[gene]])) + '\n')"""
 
+def fasta_formatter(aln, read_aln_id,feature_name, percent_id):
+	read_name = str(aln.read.name)
+	read_name_id = read_name + '.' + str(read_aln_id)
+	#print read_name_id
+	genome_name = str(aln.iv.chrom)
+	aln_start = str(aln.iv.start)
+	aln_end = str(aln.iv.end)
+	read_seq = str(aln.read.seq)
+	pct_id = str(round(percent_id,2))
+	fasta_record = str('>' + read_name + '{read_name=' + read_name_id + '}'\
+	 + '{genome=' + genome_name + '}' +'{gene=' + feature_name + '}'\
+	  + '{pct_id=' + pct_id + '}' + '{aln_start=' + aln_start + '}' + '{aln_end=' + aln_end + '}'+'\n')
+	fasta_record += read_seq + '\n'
+	return fasta_record
 
 def parse_cigar_alignment(cigar_string):
 	#:wq""
